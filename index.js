b84f5b04be83d2b2fde9e87a0816786ebeca165e
amrendergill <amrender028@gmail.com>
2021-10-18 19:54:19 +0530

    readme.md

---
+++ index.js
@@ -0,0 +1,85 @@
+const express= require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT=process.env.PORT || 5000;
+
+
+const returnScraperApiUrl = (apiKey) =>
+  `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send('Welcome to Amazon Scraper API')
+})
+
+//GEt Product details
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+
+  try {
+    const response = await request(
+      `${returnScraperApiUrl(
+        api_key
+      )}&url=https://www.amazon.in/dp/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+//GET prduct reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+
+  try {
+    const response = await request(
+      `${returnScraperApiUrl(
+        api_key)}&url=https://www.amazon.in/product-reviews/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//GET prduct offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+
+  try {
+    const response = await request(
+      `${returnScraperApiUrl(
+        api_key)}&url=https://www.amazon.in/gp/offer-listing/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//GET prduct offers
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  const { api_key } = req.query;
+
+
+  try {
+    const response = await request(
+      `${returnScraperApiUrl(
+        api_key)}&url=https://www.amazon.in/s?k=${searchQuery}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(PORT , ()=> console.log(`Server running on port: ${PORT}`));
